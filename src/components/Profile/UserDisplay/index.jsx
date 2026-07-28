a32110cf5f5463082946370f354f524353956c32
pierre-canet <pierre.canet@laplateforme.io>
2026-07-28 12:10:22 +0200

    Affichage des informations de livraison et de facturation (CustomerDisplay)

--- src/components/Profile/UserDisplay/index.jsx
+++ src/components/Profile/UserDisplay/index.jsx
@@ -1,8 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchCurrentUserThunk } from "../../../thunkActionsCreator/userThunks";
+import {
+  fetchCurrentUserThunk,
+  fetchCurrentCustomerThunk,
+} from "../../../thunkActionsCreator/userThunks";
 
-export default function UserDisplay() {
+export function UserDisplay() {
   const dispatch = useDispatch();
 
   const profile = useSelector((state) => state.user.profile);
@@ -22,6 +25,35 @@ export default function UserDisplay() {
       <p>Email : {profile.email}</p>
       <p>Prénom : {profile.firstName}</p>
       <p>Nom : {profile.lastName}</p>
+    </div>
+  );
+}
+
+export function CustomerDisplay() {
+  const customer = useSelector((state) => state.user.customer);
+  const billing = customer?.billing;
+  const shipping = customer?.shipping;
+  if (!customer) return <p>Chargement...</p>;
+  console.log("Hi Billing !");
+  console.log(billing);
+  console.log("Hi Shipping !");
+  console.log(shipping);
+  return (
+    <div>
+      <h2>Informations de facturation</h2>
+      {customer &&
+        Object.entries(billing).map(([key, value]) => (
+          <p key={key}>
+            {key} : {value}
+          </p>
+        ))}
+      <h2>Informations de livraison</h2>
+      {customer &&
+        Object.entries(shipping).map(([key, value]) => (
+          <p key={key}>
+            {key} : {value}
+          </p>
+        ))}
     </div>
   );
 }
--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,12 +1,15 @@
 import { UpdateForm } from "../../components/Profile/UpdateUser";
 import DeleteAccountButton from "../../components/DeleteAccountButton";
-import UserDisplay from "../../components/Profile/UserDisplay";
+import { UserDisplay } from "../../components/Profile/UserDisplay";
+import { CustomerDisplay } from "../../components/Profile/UserDisplay";
+
 export default function Profile() {
   return (
     <div>
       <UserDisplay />
       <UpdateForm />
       <DeleteAccountButton />
+      <CustomerDisplay />
     </div>
   );
 }
