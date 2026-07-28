d4c68b753ab39de45b63424ef164fe7c7c39de6c
pierre-canet <pierre.canet@laplateforme.io>
2026-07-28 09:37:55 +0200

    Affichage des données utilisateur dans la page profil

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,9 +1,28 @@
-import UserTest from "../../components/UserTest";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCurrentUserThunk } from "../../thunkActionsCreator/userThunks";
 
-export default function User() {
+export default function Profile() {
+  const dispatch = useDispatch();
+
+  const profile = useSelector((state) => state.user.profile);
+  const loading = useSelector((state) => state.user.loading);
+  const error = useSelector((state) => state.user.error);
+
+  useEffect(() => {
+    dispatch(fetchCurrentUserThunk());
+  }, [dispatch]);
+  if (loading) return <p>Chargement...</p>;
+  if (error) return <p>{error}</p>;
+  if (!profile) return <p>Aucun profil.</p>;
+  console.log(profile);
+
   return (
-    <>
-      <UserTest />
-    </>
+    <div>
+      <p>Nom d'utilisateur : {profile.username}</p>
+      <p>Email : {profile.email}</p>
+      <p>Prénom : {profile.firstName}</p>
+      <p>Nom : {profile.lastName}</p>
+    </div>
   );
 }
