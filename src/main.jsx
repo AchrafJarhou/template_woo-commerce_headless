15116c94819197816e4abb73582f8606282d397a
Rsoumre <harounasoumare547@gmail.com>
2026-07-28 16:08:48 +0200

    undo main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -31,7 +31,6 @@ import Store from "./pages/Store";
 import Home from "./pages/Home";
 import Login from "./pages/login";
 import Register from "./pages/register";
-import ResetPassword from "./pages/ResetPassword";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Toast from "./components/Toast";
@@ -91,7 +90,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           {/* <Route path="/" element={<Store />} /> */}
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
           {/* <Route path="/" element={<Home />} /> */}
           <Route path="/catalogue" element={<Store />} />
           <Route path="/mentions-legales" element={<MentionsLegales />} />
