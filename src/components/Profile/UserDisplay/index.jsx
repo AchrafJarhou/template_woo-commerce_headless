24790ae07c12d6c07d863b14e52f2ed05686f216
pierre-canet <pierre.canet@laplateforme.io>
2026-07-28 11:03:05 +0200

    Déplacement de la logique d'affichage des données dans un composant UserDisplay

---
+++ src/components/Profile/UserDisplay/index.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCurrentUserThunk } from "../../../thunkActionsCreator/userThunks";
+
+export default function UserDisplay() {
+  const dispatch = useDispatch();
+
+  const profile = useSelector((state) => state.user.profile);
+  const loading = useSelector((state) => state.user.loading);
+  const error = useSelector((state) => state.user.error);
+
+  useEffect(() => {
+    dispatch(fetchCurrentUserThunk());
+  }, [dispatch]);
+  if (loading) return <p>Chargement...</p>;
+  if (error) return <p>{error}</p>;
+  if (!profile) return <p>Aucun profil.</p>;
+
+  return (
+    <div>
+      <p>Nom d'utilisateur : {profile.username}</p>
+      <p>Email : {profile.email}</p>
+      <p>Prénom : {profile.firstName}</p>
+      <p>Nom : {profile.lastName}</p>
+    </div>
+  );
+}
