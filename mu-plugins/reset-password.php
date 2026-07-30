9fdb4ab9339e469c484caab2366fbc08bea7241d
petit-pierre <aubree.pierre2@gmail.com>
2026-07-30 14:21:32 +0200

    mailing wp

--- mu-plugins/reset-password.php
+++ mu-plugins/reset-password.php
@@ -8,23 +8,6 @@ add_action('rest_api_init', function () {
     ]);
 });
 
-// 1. Utiliser l'adresse "De" configurée dans WooCommerce pour TOUS les mails du site
-add_filter('wp_mail_from', function ($original_email) {
-    $wc_email = get_option('woocommerce_email_from_address');
-
-    // Si l'option WooCommerce existe et n'est pas vide, on l'utilise
-    return !empty($wc_email) ? $wc_email : $original_email;
-});
-
-// 2. Utiliser le nom "De" configuré dans WooCommerce
-add_filter('wp_mail_from_name', function ($original_name) {
-    $wc_name = get_option('woocommerce_email_from_name');
-
-    return !empty($wc_name) ? $wc_name : get_bloginfo('name');
-});
-
-
-
 function headless_reset_password($request)
 {
     $email = sanitize_email($request->get_param('email'));
