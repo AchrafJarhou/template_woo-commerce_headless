cc8a3f59f46dfddb4b8f88020e93724e5e55f7c5
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2026-08-05 12:43:18 +0200

    Ajout du Header custom pour la page Home et masquage des éléments globaux

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,3 +1,11 @@
+import Header from "./components/Header/Header";
+import styles from "./MainLayout.module.scss";
+
 export default function MainLayout({ children }) {
-  return <>{children}</>;
+  return (
+    <div className={styles.layout}>
+      <Header />
+      {children}
+    </div>
+  );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import MainLayout from "../../layouts/MainLayout/MainLayout";
 import HeroVideo from "../../components/Home/HeroVideo/HeroVideo";
 import CatalogSection from "../../components/Home/CatalogSection/CatalogSection";
@@ -5,6 +6,12 @@ import { PRODUCTS } from "../../components/Home/data/products";
 import styles from "./Home.module.scss";
 
 export default function Home() {
+  useEffect(() => {
+    // Ajouter classe pour masquer le Header/Footer globaux
+    document.body.classList.add("home-page");
+    return () => document.body.classList.remove("home-page");
+  }, []);
+
   return (
     <MainLayout>
       <div className={styles.home}>
