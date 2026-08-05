bfe1bcfcbb9991e2b5428581fc3c9d1ad277f3fd
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2026-08-05 12:31:48 +0200

    Création du composant CatalogSection (conteneur du catalogue)

---
+++ src/components/Home/CatalogSection/CatalogSection.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import FilterBar from "../FilterBar/FilterBar";
+import ProductGrid from "../ProductGrid/ProductGrid";
+import Footer from "../Footer/Footer";
+import styles from "./CatalogSection.module.scss";
+
+export default function CatalogSection({ products }) {
+  const [filter, setFilter] = useState("tous");
+
+  return (
+    <div className={styles.catalog}>
+      <main className={styles.main}>
+        <FilterBar onFilterChange={setFilter} />
+        <ProductGrid products={products} filter={filter} />
+      </main>
+      <Footer />
+    </div>
+  );
+}
