a5de71e4b817cc6efbb7e45737cbebf6151a8847
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2026-08-05 12:33:16 +0200

    Mise à jour du export de la page Home vers le nouveau composant

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,1 @@
-import HomeSlider from "../../components/HomeSlider";
-import PageContent from "../../components/PageContent";
-import DeleteAccountButton from "../../components/DeleteAccountButton";
-
-export default function Home() {
-  return (
-    <div className="home">
-      <PageContent slug="home" />
-      <HomeSlider />
-      <PageContent slug="a-propos" />
-    </div>
-  );
-}
+export { default } from "./Home";
