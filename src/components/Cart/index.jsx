ebffb898aec7a80ccb4a9a735834474783634250
Dalin James <charlestedison@gmail.com>
2026-09-01 14:53:25 +0200

    refactor (Cart): changement du code en mode statique pour tester l'intégration

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,33 +1,48 @@
-import { useSelector, useDispatch } from "react-redux";
 import { CartProduct } from "../CartProduct";
-import { emptyCartThunk } from "../../thunkActionsCreator/cartThunks";
-import Coupon from "../Coupon";
-import StripeWrapper from "../StripeWrapper";
+import { formatPrice } from "../../utils/formatPrice";
+import { MOCK_CART_ITEMS, MOCK_CART_TOTALS } from "./mockCart";
 
 export default function Cart() {
-  const items = useSelector((state) => state.cart.items);
-  const totals = useSelector((state) => state.cart.totals);
-  const dispatch = useDispatch();
+  const items = MOCK_CART_ITEMS;
+  const totals = MOCK_CART_TOTALS;
+
+  if (items.length === 0) {
+    return (
+      <section className="cart cart--empty">
+        <p className="cart__empty-message">Votre panier est vide.</p>
+      </section>
+    );
+  }
 
   return (
-    <>
-      <div>Votre Panier</div>
-      <ul>
+    <section className="cart">
+      <ul className="cart__list">
         {items.map((item) => (
           <CartProduct key={item.key} item={item} />
         ))}
       </ul>
 
-      <Coupon />
+      <dl className="cart__summary">
+        <div className="cart__summary-row">
+          <dt>Sous-total</dt>
+          <dd>{formatPrice(totals.total_items, totals)}</dd>
+        </div>
 
-      <div>
-        Total:{" "}
-        {totals &&
-          (parseInt(totals.total_price) / 100).toFixed(2) +
-            totals.currency_suffix}
-      </div>
-      <button onClick={() => dispatch(emptyCartThunk())}>Vider Panier</button>
-      <StripeWrapper></StripeWrapper>
-    </>
+        <div className="cart__summary-row">
+          <dt>Livraison</dt>
+          <dd>Calculée à l'étape suivante</dd>
+        </div>
+
+        <div className="cart__summary-row">
+          <dt>Taxes</dt>
+          <dd>{formatPrice(totals.total_tax, totals)}</dd>
+        </div>
+
+        <div className="cart__summary-row cart__summary-row--total">
+          <dt>Total</dt>
+          <dd>{formatPrice(totals.total_price, totals)}</dd>
+        </div>
+      </dl>
+    </section>
   );
 }
