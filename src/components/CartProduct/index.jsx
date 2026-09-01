074a696b6a855cdf0cf883f5989bc3f0cef13fce
Dalin James <charlestedison@gmail.com>
2026-09-01 14:12:52 +0200

    refactor (CartProduct): refactorisation pour une meilleure adaptation en fonction du template

--- src/components/CartProduct/index.jsx
+++ src/components/CartProduct/index.jsx
@@ -1,89 +1,62 @@
-import {
-  addProductToCart,
-  deleteProductFromCart,
-  substractProductFromCart,
-} from "../../thunkActionsCreator/cartThunks";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import "./index.css";
+import { formatPrice } from "../../utils/formatPrice";
+import "./index.scss";
 
 export function CartProduct({ item }) {
-  const dispatch = useDispatch();
+  const urlParts = item.permalink.split("/");
+  const slug = urlParts[urlParts.length - 2];
 
-  const url_array = item.permalink.split("/");
-  const slug = url_array[url_array.length - 2];
+  const atMinimum = item.quantity <= item.quantity_limits.minimum;
+  const atMaximum = item.quantity >= item.quantity_limits.maximum;
 
   return (
-    <li className="cart-product-list">
-      <Link to={/product/ + slug}>
+    <li className="cart-product">
+      <Link to={`/product/${slug}`} className="cart-product__media">
         <img
-          className="cart-product-thumbnail"
-          src={
-            item.images?.[0]?.thumbnail ||
-            "https://placeholder.pics/svg/300/DEDEDE/555555/Produit%20sans%20illustration"
-          }
-          alt={item.name || "produit sans nom"}
-        ></img>
-        <p>{item.name || "produit sans nom"}</p>
-        <span
-          dangerouslySetInnerHTML={{
-            __html:
-              item.short_description ||
-              item.description ||
-              "pas de description",
-          }}
-        ></span>
-        {item.variation &&
-          item.variation.map((variation) => (
-            <p key={variation.attribute}>
-              {variation.attribute} : {variation.value}
-            </p>
-          ))}
-        <p>{item.quantity}</p>
-        {item.prices && (
-          <p>
-            {(parseInt(item.prices.price) / 100).toFixed(2) +
-              item.prices.currency_suffix}
+          className="cart-product__thumbnail"
+          src={item.images?.[0]?.thumbnail}
+          alt={item.images?.[0]?.alt || item.name}
+        />
+      </Link>
+
+      <div className="cart-product__body">
+        <Link to={`/product/${slug}`} className="cart-product__name">
+          {item.name}
+        </Link>
+
+        {item.variation.length > 0 && (
+          <p className="cart-product__variation">
+            {item.variation
+              .map((detail) => `${detail.attribute} : ${detail.value}`)
+              .join(" · ")}
           </p>
         )}
-      </Link>
-      <button
-        disabled={item.quantity === item.quantity_limits.maximum}
-        onClick={() => {
-          dispatch(
-            addProductToCart({
-              productId: item.id,
-              quantity: 1,
-              variation: item.variation?.[0] || [],
-            }),
-          );
-        }}
-      >
-        Ajouter +
-      </button>
-      <button
-        onClick={() => {
-          dispatch(
-            substractProductFromCart({
-              itemKey: item.key,
-              quantity: item.quantity,
-            }),
-          );
-        }}
-      >
-        Reduire -
-      </button>
-      <button
-        onClick={() => {
-          dispatch(
-            deleteProductFromCart({
-              itemKey: item.key,
-            }),
-          );
-        }}
-      >
-        Supprimer
-      </button>
+
+        <div className="cart-product__quantity">
+          <span className="cart-product__quantity-label">Qté</span>
+          <button
+            type="button"
+            className="cart-product__step"
+            disabled={atMinimum}
+            aria-label={`Retirer un ${item.name}`}
+          >
+            −
+          </button>
+          <span className="cart-product__quantity-value">{item.quantity}</span>
+          <button
+            type="button"
+            className="cart-product__step"
+            disabled={atMaximum}
+            aria-label={`Ajouter un ${item.name}`}
+          >
+            +
+          </button>
+        </div>
+      </div>
+
+      <p className="cart-product__price">
+        {formatPrice(item.totals.line_total, item.totals)}
+      </p>
     </li>
   );
 }
