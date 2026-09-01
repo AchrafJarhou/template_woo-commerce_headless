b96d423f8160d44f2f38103e1cf4137e0b54a337
Dalin James <charlestedison@gmail.com>
2026-09-01 14:07:51 +0200

    refactor (utils): suppression de la ligne de code sur le currency EUR

--- src/utils/formatPrice.js
+++ src/utils/formatPrice.js
@@ -1,6 +1,5 @@
 export function formatPrice(amount, currency) {
   const minorUnit = currency?.currency_minor_unit ?? 2;
-  const code = currency?.currency_code ?? "EUR";
   const value = Number(amount) / 10 ** minorUnit;
 
   return new Intl.NumberFormat("fr-FR", {
