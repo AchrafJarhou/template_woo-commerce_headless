5586ff9463c93930297997c8502829a65ce40977
Dalin James <charlestedison@gmail.com>
2026-09-01 11:32:44 +0200

    feat: ajout du code pour le format de la monnaie européenne
    Par exemple, les nombres seront écrit avec ce format exacte => 1 340,00 €

---
+++ src/utils/formatPrice.js
@@ -0,0 +1,10 @@
+export function formatPrice(amount, currency) {
+  const minorUnit = currency?.currency_minor_unit ?? 2;
+  const code = currency?.currency_code ?? "EUR";
+  const value = Number(amount) / 10 ** minorUnit;
+
+  return new Init.NumberFormat("fr-FR", {
+    style: "currency",
+    currency: code,
+  }).format(value);
+}
