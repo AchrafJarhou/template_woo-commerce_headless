f825097d930a664cec63a5788fdc2efcba55213d
Dalin James <charlestedison@gmail.com>
2026-09-02 23:50:27 +0200

    refactor : dans le CatalogueSection, j'ai enlever l'import du footer. Dans le second fichier qui est le MainLayout, j'ai changé l'import parce que j'ai modifier l'emplacement du dossier Footer

--- src/components/Home/CatalogSection/CatalogSection.jsx
+++ src/components/Home/CatalogSection/CatalogSection.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Header from "../../../layouts/MainLayout/components/Header/Header";
 import FilterBar from "../FilterBar/FilterBar";
 import ProductGrid from "../ProductGrid/ProductGrid";
-import Footer from "../Footer/Footer";
 import styles from "./CatalogSection.module.scss";
 import ProductModal from "../../ProductModal";
 
@@ -30,7 +29,6 @@ export default function CatalogSection({ products }) {
         />
       )}
 
-      <Footer />
     </div>
   );
 }
