90ca6417db7b7bf9ef52a73ea6d17eb482023dbd
Dalin James <charlestedison@gmail.com>
2026-09-02 22:45:23 +0200

    refactor: changement minime dans le scss de Cart Suppression du fichier HeroSentinelContext. Nous sommes revenus en arrière pour les modificaitons du code. Ce fichier ne sert plus à rien.

--- src/layouts/MainLayout/HeroSentinelContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const HeroSentinelContext = createContext(null);
-
-const noop = () => {};
-
-export function useHeroSentinel() {
-  return useContext(HeroSentinelContext) ?? noop;
-}
