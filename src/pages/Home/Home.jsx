d7c53cd67dfdd0c7cde2e049ad890ac4e35e76be
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2026-09-10 12:02:58 +0200

    feat: ajouter le bouton Voir plus pour la pagination des produits
    Remplace la pagination classique par un bouton Voir plus qui charge
    progressivement 20 produits supplementaires a chaque clic.
    
    Changements:
    - Home.jsx: gere le chargement progressif des pages
    - CatalogSection: affiche le bouton Voir plus et le spinner au chargement
    - productSlice: accumule les produits au lieu de les remplacer
    - productsThunks: recupere le total de produits via le header X-WP-Total
    - Styles: bouton noir avec texte blanc, responsive
    
    Le spinner saffiche quand lAPI charge pour eviter le message aucun produit.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import HeroVideo from "../../components/Home/HeroVideo/HeroVideo";
 import CatalogSection from "../../components/Home/CatalogSection/CatalogSection";
@@ -7,8 +7,11 @@ import styles from "./Home.module.scss";
 
 export default function Home() {
   const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
   const products = useSelector((state) => state.products.list.data);
   const filters = useSelector((state) => state.filters);
+  const totalProducts = useSelector((state) => state.products.list.total);
+  const loading = useSelector((state) => state.products.loading);
 
   useEffect(() => {
     document.body.classList.add("home-page");
@@ -17,12 +20,26 @@ export default function Home() {
 
   useEffect(() => {
     dispatch(fetchProductsThunk({ ...filters, page: 1, per_page: 20 }));
+    setCurrentPage(1);
   }, [filters, dispatch]);
+
+  const handleLoadMore = () => {
+    const nextPage = currentPage + 1;
+    dispatch(fetchProductsThunk({ ...filters, page: nextPage, per_page: 20 }));
+    setCurrentPage(nextPage);
+  };
+
+  const hasMore = products.length < totalProducts;
 
   return (
     <div className={styles.home}>
       <HeroVideo />
-      <CatalogSection products={products || []} />
+      <CatalogSection
+        products={products || []}
+        hasMore={hasMore}
+        loading={loading}
+        onLoadMore={handleLoadMore}
+      />
     </div>
   );
 }
